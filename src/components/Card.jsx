b88bc8a777637eb7db83cdbbdcb3ba96f2af7703
Avir Karakitsos <avir.karakitsos@gmail.com>
2023-12-24 12:38:26 +0100

    refactor: card containers

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,11 +5,9 @@ function Card({content}) {
     return(
         <article className="cardContainer">
             <section className="cardContainer__left">
-                <div className="subContainer">
-                    <img src={content.logo} alt="logo microsoft" className={content.title}/>
-                    <div className="cardContainer__left__content">
-                        <p>{content.role}</p>
-                    </div>
+                <img src={content.logo} alt="logo microsoft" className={content.title}/>
+                <div className="cardContainer__left__content">
+                    <p>{content.role}</p>
                 </div>
             </section>
             <section className="cardContainer__right">
